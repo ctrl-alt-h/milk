dbef3bc8d74147ba31e5241063410198f869126d
jonathan-henning <jonathanchenning@outlook.com>
2025-12-08 13:41:41 -0500

    initial framework for parsing arguments

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include "tracking.h"
+#include <stdio.h>
+
+void mk_print_help(void) {
+	printf("Usage: milk <options>\n");
+}
 
 int main(int argc, char **argv) {
-	int ticket_counter[MK_TICKET_TYPE_COUNT];
+	if (argc <= 1) {
+		mk_print_help();
+		return 0;
+	}
 
+	int ticket_counter[MK_TICKET_TYPE_COUNT];
 	for (int i = 0; i < MK_TICKET_TYPE_COUNT; ++i) {
 		ticket_counter[i] = 0;
 	}
 
-	mk_inc_counter(ticket_counter, MK_TICKET_TYPE_WATER_DAMAGE);
-	mk_print_status(ticket_counter, MK_TICKET_TYPE_COUNT);
+	// TODO: Parse arguments
 }
--- src/tracking.h
+++ src/tracking.h
@@ -8,8 +8,6 @@ enum MK_TICKET_TYPE {
 	MK_TICKET_TYPE_COUNT
 };
 
-static char *MK_TICKET_STRINGS[MK_TICKET_TYPE_COUNT];
-
 void mk_inc_counter(int *ticket_counter, int ticket_type);
 void mk_dec_counter(int *ticket_counter, int ticket_type);
 void mk_print_status(const int *ticket_counter, const int size);
