b73dbbff9c642c31319997924967e505479ae041
Jonathan Henning <jonathanchenning@outlook.com>
2025-12-18 16:25:47 -0500

    docs

--- src/context.h
+++ src/context.h
@@ -1,13 +1,18 @@
+// context.h - App state and common type definitions
+// Copyright (C) 2025, Jonathan Henning
+
 #ifndef _CONTEXT_H
 #define _CONTEXT_H
 
 #include "stddef.h"
 
+/* ----- TYPES ----- */
 struct mk_ticket_cat {
 	const char  *title;
 	unsigned int count;
 };
 
+/* ----- APP STATE ----- */
 struct mk_context {
 	struct mk_ticket_cat *ticket_cats;
 	size_t n_ticket_cats;
